bac55b1ca179923390de1687fa9c8624939ceb7e
Lu1sValenc1a <ldquartz@hotmail.com>
2024-06-08 17:25:21 -0600

    new file:   .gitignore     new file:   .vscode/extensions.json 	new file:   include/README 	new file:   lib/README 	new file:   platformio.ini 	new file:   src/main.cpp 	new file:   test/README

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+#define liquidsensorPin 11
+#define pumpwaterPin 9
+#define delayt
+
+void llenado(){
+  int valorliquidSensor = digitalRead(11);
+  if (valorliquidSensor == 0)
+  {
+    digitalWrite(9, LOW);
+  }else if (valorliquidSensor == 1)
+  {
+    digitalWrite(9, HIGH);
+  }
+  
+}
+
+void setup() {
+  pinMode(11, INPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+  llenado();
+}
